8978f479d81ada09a59d638d461dd2eeac948920
sunsetkk <khyejin9796@gmail.com>
2025-04-24 19:47:05 +0900

    first commit

---
+++ java/2025_04_24/Main.java
@@ -0,0 +1,15 @@
+// 문제 제목: [문제 제목]
+// 링크: [문제 링크]
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력 받기
+
+        // 풀이 로직
+
+        // 출력
+    }
+}
